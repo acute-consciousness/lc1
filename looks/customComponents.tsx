f3ebfa2d1cedb90478b78ff2b7043fb3930271c4
acute-consciousness <erickmathai@gmail.com>
2026-08-18 16:29:53 +0300

    white screen of death
    will solve later when i come back

--- looks/customComponents.tsx
+++ looks/customComponents.tsx
@@ -2,15 +2,41 @@ import {View,Text,TextInput,StyleSheet, KeyboardAvoidingView, Platform, Touchabl
 import { Colours } from './Colours';
 import { ReactNode } from 'react';
 /**
+ * waaahh, what a hustle that was. what a hustle that was
+ * i'll reflect on it later. i'll, i'll, i'll reflect in it later, now
+ * what the disable thing
+ * now where will it be , to me it must be in the account file as, as, as, as what
+ * as the state thing thins and stuff. Walai, walai, walai, walai javascript
  */
  interface TextInputProps {
-    label:ReactNode;
-        placeHolder:string;
-        value:string;
-        onChangeText:(text:string)=>void;//huku ni mazingaombwe, but guess it represents/ is a shortcut of retrun void, naah, ni mazingaombwe(arrow functions and the lot)
-        onSubmitEditing:(e:TextInputSubmitEditingEvent)=>void;// type function
-        touchableText:string;
+    label?:ReactNode;// just remiding myself, just reminding myself of, myself of ReactNode type element
+    // ah, natukiweza pitisha onChange, mmh, onchange hapa. zii, juu what we want is it's value
+        placeHolder?:string;
+        value?:string;
+        onChangeText?:(e:any)=>void;//huku ni mazingaombwe, but guess it represents/ is a shortcut of retrun void, naah, ni mazingaombwe(arrow functions and the lot). void? hadi, void?, void?, void? hadi kama, hadi kama itarudisha vitu? Mazinga, mazingaobwe braaH
+        onSubmitEditing?:(e:any)=>void;// type function
+        touchableText?:string;
+        disabled?:boolean;//nikienka onChange, mmh, nikieka onChange hapa, it, it, it says it, it says it refers, it refers to a value
+        // walai, walai, walai, walai javascript
     }
+
+   const CustomTouchableOpacity = (props:TextInputProps)=> {
+        return (
+            // nothing related to what is below, disable property. bpp - that it can be done even on, on, on, on a touchable
+            // onPress and opacity. i think onPress is for clickable, ok, we, ok, we will play with them
+            // sasa, sasa, sasa, sasa ukule tucall, sasa,sasa ukuje tucall iyou function huku
+            // how do we go about that
+            // bila hadi kuangalia further, mmh, i think i know where i'm going wrong. it from, it's, mmh, it's from the understanding
+            /* what did i, what did i want to say, jsut check if disable works, works and, and, and, and, and check out what it does
+            but nilikuwa nataka kusema, the value true or false can just be passed in the accounts file
+            changing the style, changing, mmh, changing, mmmh, changing the styles is what, is what, is what brought me here*/
+   <TouchableOpacity style={styles.btn} disabled={props.disabled}>//what about props.onchange. But we haven't defined this
+                    <Text style={styles.btnText}>{props.touchableText}</Text>
+                </TouchableOpacity>
+
+        )
+    }
+
    const CustomTextInput = (props:TextInputProps) =>{
         return(
         <View style={styles.viewMain}>
@@ -33,16 +59,14 @@ import { ReactNode } from 'react';
             </View>
 
             <View style={styles.viewThree}>
-                <TouchableOpacity style={styles.btn}>
-                    <Text style={styles.btnText}>{props.touchableText}</Text>
-                </TouchableOpacity>
-
+               <CustomTouchableOpacity 
+               disabled={props.disabled} 
+               touchableText={props.touchableText}/>
             </View>
 
             </View>
 
             </KeyboardAvoidingView>
-
             </View>
         )
 
--- screens/Account.tsx
+++ screens/Account.tsx
@@ -21,8 +21,39 @@ export default function Account(){
     const [text,setText]=useState('');//maybe it's an empty string <--this is just an extension of my mind talking
     const onSubmit = (e:TextInputSubmitEditingEvent)=>{ // even that native can tell javascript, javascript, javascript ,mmh, that i has been hit
         const nowSnapshot = e.nativeEvent.text;//replace text with value, let as name value text. remember we don't use the setState identifier
-        console.log(nowSnapshot);
+        console.log(nowSnapshot);// imehold iyo kitu, the whole keyboard, the, the, the whole keyboard text inputted
+        
+
     }
+    const [disable, setEnable]=useState<boolean>(true);
+    const onChange = (ch:string) =>{
+        const input:string = ch;
+        setText(input);
+        while (input.length==10) {
+            if(input.length==10){
+            setEnable(false); //hapa, hapa, hapa, hapa ndio, hapa ndio nilifika
+             // jana, and, and, and i remember what asking how data went back to a child with props and i don't want
+             // to use state even, state, state, state, if, if, if, if it's even possible
+            //  a function was the bpp, a, a, a, a, a function was the bpp answer
+            // ok, a function, but how do we, but, but, but how do we even do that?
+            return false;
+            }
+            else(input.length!=10)
+            { 
+                setEnable(true)//{disable}; <-this, this, this was me
+        }
+        {/*how did we track the, how, how, how, how did we track the character back then
+            we had split('with a delimeter', this had the characters in arrays
+            , we can actually do split, but maybe char??
+            map mapped them into pizza emoji's
+            join...), so , so, so what do we, what do we, what do we learn from this
+            what, what about the event thing
+           so, i checked it up, i , i, i checked it up and we .length the thing we were told 
+           .length, dot length, dot, dot length, that, that, that, that is where we were yesterday
+           
+            */} 
+    }
+
     return(
         <View style={styles.one}> 
         <View style={styles.two}>
@@ -30,9 +61,15 @@ export default function Account(){
             label={<Text>enter mobile number</Text>}
             placeHolder='+254 *********' 
             value={text}
-            onChangeText={setText}//before checking on the error we are being flagged, at last after the 'done' is hit, that is when, that is when what, the user input will be set to state?
+            // onChangeText={onChange}// it will register it, it will, it will, it will, it will register after, after it is done
+            // onChange, if it was a functon, if it was a function, if, mmh, if, mmh, if it was a function
+            // fingekuwa, ingekuwa function identifier(char ch){},we, we, we learnt something about onChangeText when covering split and stuff
+            // (text) => {something is assigned here}
+            //let us use split
+            //before checking on the error we are being flagged, at last after the 'done' is hit, that is when, that is when what, the user input will be set to state?
             onSubmitEditing={onSubmit}
-            touchableText='continue'
+              touchableText='continue'
+              disabled={disable}
             /> 
             <View>
                 <Text></Text>
@@ -49,3 +86,4 @@ const styles = StyleSheet.create({
         margin:10,
     }
 })
+}
